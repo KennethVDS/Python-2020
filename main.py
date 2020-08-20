36ec22d923f44bb7264b712747051576827a8375
Kenneth <kennethvdstrieckt@hotmail.com>
2020-08-20 13:48:41 +0200

    basics2

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-name = input ('What is your name?')
-print(name + 'you sexy motherfucker')
+username = input ('What is your name?')
+password = input('What is your password?')
+
+password_length = len(password)
+hidden_password = '*' * password_length
+
+print(f'{username}, you sexy motherfucker. Your password, {hidden_password}, is {password_length} letters long')
